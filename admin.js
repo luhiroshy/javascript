5ca15ba3d5408b8bffb547cb00ca7a4e5f85c72c
Luan Hiroshy <164904163+luhiroshy@users.noreply.github.com>
2026-03-17 11:49:37 -0300

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,12 @@
+let usercerto = "admin";
+let senhacerta = "1234";
+let vezes = 1;
+
+while (vezes <= 3) {
+    let user = prompt("Digite o usuário: ");
+    let senha = prompt("Digite a senha: ");
+    
+    if (user === usercerto && senha === senhacerta) {
+        console.log("Usuário e senha correta");
+}
+    }
