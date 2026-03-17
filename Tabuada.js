814359ae48ec66d33383d5393528a3a1261ea054
Luan Hiroshy <164904163+luhiroshy@users.noreply.github.com>
2026-03-17 07:47:57 -0300

    Add multiplication table for user input number

---
+++ Tabuada.js
@@ -0,0 +1,13 @@
+let n = Number(prompt("Escolha um número: "));
+let r1 = 1 * n;
+let r2 = 2 * n;
+let r3 = 3 * n;
+let r4 = 4 * n;
+let r5 = 5 * n;
+let r6 = 6 * n;
+let r7 = 7 * n;
+let r8 = 8 * n;
+let r9 = 9 * n;
+let r10 = 10 * n;
+
+console.log("A tabuada desse número de 1 a 10 é; \n1*" + n + "= " + r1 + "\n2*" + n + "= " + r2 + "\n3*" + n + "= " + r3 + "\n4*" + n + "= " + r4 + "\n5*" + n + "= " + r5 + "\n6*" + n + "= " + r6 + "\n7*" + n + "= " + r7 + "\n8*" + n + "= " + r8 + "\n9*" + n + "= " + r9 + "\n10*" + n + "= " + r10);
